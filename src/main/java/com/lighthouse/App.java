c8b8d182c6a87ccb8095b373ab3a433dff080c83
wei jie wang <dlwangwj@cn.ibm.com>
2018-03-09 21:55:45 +0800

    commit

--- src/main/java/com/lighthouse/App.java
+++ src/main/java/com/lighthouse/App.java
@@ -1,13 +1,28 @@
 package com.lighthouse;
 
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+
 /**
  * Hello world!
  *
  */
-public class App 
+@SpringBootApplication
+public class App extends SpringBootServletInitializer 
 {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(App.class);
+	}
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	SpringApplication application = new SpringApplication(App.class);
+        application.setBannerMode(Banner.Mode.OFF); 
+        application.run(args); 
+        System.out.println( "application run" );
     }
 }
